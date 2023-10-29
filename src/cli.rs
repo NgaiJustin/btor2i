cd08b268c77a9d24addd430b19275daadfa9d6cd
Justin Ngai <jn537@cornell.edu>
2023-10-28 22:35:53 -0400

    factor out cli functionality

---
+++ src/cli.rs
@@ -0,0 +1,15 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(about, version, author)] // keeps the cli synced with Cargo.toml
+#[command(allow_hyphen_values(true))]
+pub struct CLI {
+  
+  /// The BTOR2 file to run. stdin is assumed if file is not provided
+  #[arg(short, long, action)]
+  pub file: String,
+
+  /// Arguments for the main function
+  #[arg(action)]
+  pub args: Vec<String>,
+}
--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,10 @@ use btor2tools::Btor2Parser;
 use clap::Parser;
 use std::path::Path;
 
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    // /// Name of the person to greet
-    // #[arg(short, long)]
-    // name: String,
-
-    /// The BTOR2 file to run. stdin is assumed if file is not provided
-    #[arg(short, long, action)]
-    pub file: String,
-
-    // /// Number of times to greet
-    // #[arg(short, long, default_value_t = 1)]
-    // count: u8,
-}
+mod cli;
 
 fn main() {
-    let args = Args::parse();
+    let args = cli::CLI::parse();
 
     let btor2_file = Path::new(args.file.as_str());
 
