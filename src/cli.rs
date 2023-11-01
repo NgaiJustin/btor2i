df8442da8f3ade20e32ae57ccd508410ec141cfd
Justin Ngai <jn537@cornell.edu>
2023-11-01 16:19:56 -0400

    Add rustfmt.toml and format src directory

--- src/cli.rs
+++ src/cli.rs
@@ -4,7 +4,6 @@ use clap::Parser;
 #[command(about, version, author)] // keeps the cli synced with Cargo.toml
 #[command(allow_hyphen_values(true))]
 pub struct CLI {
-  
   /// The BTOR2 file to run. stdin is assumed if file is not provided
   #[arg(short, long, action)]
   pub file: String,
