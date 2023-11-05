554d480d4937587e8a8a285eb97969d99d9a8180
SanjitBasker <64987259+SanjitBasker@users.noreply.github.com>
2023-11-05 11:21:54 -0500

    more ops implemented

--- src/bvec.rs
+++ src/bvec.rs
@@ -1,3 +1,7 @@
+use std::ops::Add;
+
+
+
 #[derive(Debug, Clone)]
 pub struct BitVector<T> {
   // length of bit vector
@@ -13,8 +17,13 @@ pub struct BitVector<T> {
 }
 
 impl BitVector<u64> {
+    /// helper function to be used when changing width to [n]
     fn reserve(&mut self, n: u64) {
-        let goal_length: usize = (n / 64).try_into().unwrap();
+        let mut goal_length: usize = (n / 64).try_into().unwrap();
+        
+        if n % 64 != 0 {
+            goal_length += 1;
+        }
 
         while self.bits.len() < goal_length {
             self.bits.push(0);
@@ -22,19 +31,70 @@ impl BitVector<u64> {
     }
 
     fn get(&self, i: u64) -> bool{
-        assert! (0 <= i && i < self.width);
+        assert! (i < self.width);
         let idx: usize = (i / 64).try_into().unwrap();
         self.bits[idx] & (1 << (i % 64)) != 0
     }
 
     fn set(&mut self, i: u64, b: bool) {
-        assert! (0 <= i && i < self.width);
+        assert! (i < self.width);
         let idx: usize = (i / 64).try_into().unwrap();
-        if b {
-            self.bits[idx] |= 1 << (i % 64);
-        } else {
-            self.bits[idx] &= !(1 << (i % 64));
+
+        if b ^ self.get(i) {
+            self.bits[idx] ^= 1 << (i % 64);
         }
+    }
+
+    fn to_bytes(&self, signed: bool) -> Vec<u8> {
+        let mut bytes: Vec<u8> = Vec::new();
+
+        // handle all but the last block
+        for i in 0 .. (self.width / 64 - 1).try_into().unwrap() {
+            for j in (0 as u64) .. 7 {
+                let mask : u64 = (u64::MAX >> (8 * j)) << (8 * j);
+                bytes.push(((self.bits[i] & mask) >> (8 * j)).try_into().unwrap());
+            }
+        }
+
+        // handle the last block, except for the last byte
+        let i : usize = (self.width / 64).try_into().unwrap();
+        for j in 0 .. (self.width % 8 - 1).try_into().unwrap() {
+            let mask : u64 = (u64::MAX >> (8 * j)) << (8 * j);
+            let byte = (self.bits[i] & mask) >> (8 * j);
+            bytes.push(byte.try_into().unwrap());
+        }
+
+
+        // handle the last byte
+        let j: usize = (self.width % 8 - 1).try_into().unwrap();
+        let mask : u64 = (u64::MAX >> (8 * j)) << (8 * j);
+        let mut last_byte = (self.bits[i] & mask) >> (8 * j);
+
+        if signed {
+            for k in self.width % 8 .. 8 {
+                last_byte |= 1 << k;
+            }
+        }
+        bytes.push(last_byte.try_into().unwrap());
+
+        bytes
+    }
+
+    fn from_bytes(bytes: Vec<u8>, width: u64, signed : bool) -> BitVector<u64> {
+        let mut ans: BitVector<u64> = BitVector { width: width, bits: Vec::new() };
+        ans.reserve(width);
+        for i in 0 .. width {
+            let idx: usize = (i / 8).try_into().unwrap();
+            if idx < bytes.len() {
+                ans.set(i, bytes[idx] & (1 << (i % 8)) != 0);
+            }
+            else if i > 0 && signed {
+                ans.set(i, ans.get(i - 1));
+            } else {
+                ans.set(i, false);
+            }
+        }
+        ans
     }
 
 
@@ -65,5 +125,75 @@ impl BitVector<u64> {
             ans.set(i - (l - 1), self.get(i));
         }
         ans
+    }
+
+    fn not(&self) -> BitVector<u64> {
+        let mut ans = self.clone();
+        for i in 1..(self.width - 1) {
+            ans.set(i, !ans.get(i));
+        }
+        ans
+    }
+
+    fn inc(&self) -> BitVector<u64> {
+        let mut ans = self.clone();
+        let i: usize = 0;
+        let mut done = false;
+
+        let last_idx = (self.width / 64 - 1).try_into().unwrap();
+
+        // set all blocks to 0 which are 1 below their max.
+        // Rust overflow is UB so have to check for overflow first.
+        // if a block won't overflow, increment it and finish.
+        while i < last_idx && !done {
+            if ans.bits[i] == u64::MAX - 1 {
+                ans.bits[i] = 0;
+            }
+            else {
+                ans.bits[i] += 1;
+                done = true;
+            }
+        }
+
+        // try to increment last block. if it would fail, zero it instead.
+        if !done {
+            let last_size = self.width - ((self.width / 64 - 1)) * 64; // number of bits in last block, could be 64
+            let mut max_val: u64 = 0;
+            for i in 0..(last_size - 1) {
+                max_val |= 1 << i;
+            }
+            if ans.bits[last_idx] == max_val {
+                ans.bits[last_idx] = 0;
+            } else {
+                ans.bits[last_idx] += 1;
+            }
+        }
+        ans
+    }
+
+    fn dec(&self) {
+
+    }
+
+    fn redand(&self) {
+
+    }
+
+    fn redor(&self) {
+
+    }
+
+    fn redxor(&self) {
+
+    }
+
+    fn add(left : BitVector<u64>, right : BitVector<u64>) -> BitVector<u64> {
+        let left_bytes : Vec<u8> = left.to_bytes(true);
+        let right_bytes : Vec<u8> = right.to_bytes(true);
+        let ans : Vec<u8> = num_bigint::BigInt::from_signed_bytes_le(left_bytes.as_ref()).add(
+            num_bigint::BigInt::from_signed_bytes_le(right_bytes.as_ref())
+        ).to_signed_bytes_le();
+        return BitVector::<u64>::from_bytes(ans, left.width, true);
+
     }
 }
