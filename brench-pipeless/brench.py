354714fe1d4770f9c6f49815640999361a928b7f
Justin Ngai <jn537@cornell.edu>
2023-12-05 19:33:45 -0500

    format brench

--- brench-pipeless/brench.py
+++ brench-pipeless/brench.py
@@ -11,9 +11,9 @@ import os
 from concurrent import futures
 import glob
 
-__version__ = '1.0.0'
+__version__ = "1.0.0"
 
-ARGS_RE = r'ARGS: (.*)'
+ARGS_RE = r"ARGS: (.*)"
 
 
 def run_nopipe(cmds, timeout):
@@ -41,27 +41,22 @@ def run_nopipe(cmds, timeout):
 
 
 def run_bench(pipeline, fn, timeout):
-    """Run a single benchmark pipeline.
-    """
+    """Run a single benchmark pipeline."""
     # Load the benchmark.
     with open(fn) as f:
         in_data = f.read()
 
     # Extract arguments.
     match = re.search(ARGS_RE, in_data)
-    args = match.group(1) if match else ''
+    args = match.group(1) if match else ""
 
     # Run pipeline.
-    cmds = [
-        c.format(args=args, file_path=fn)
-        for c in pipeline
-    ]
+    cmds = [c.format(args=args, file_path=fn) for c in pipeline]
     return run_nopipe(cmds, timeout)
 
 
 def get_result(strings, extract_re):
-    """Extract a group from a regular expression in any of the strings.
-    """
+    """Extract a group from a regular expression in any of the strings."""
     for s in strings:
         match = re.search(extract_re, s)
         if match:
@@ -70,41 +65,44 @@ def get_result(strings, extract_re):
 
 
 @click.command()
-@click.option('-j', '--jobs', default=None, type=int,
-              help='parallel threads to use (default: suitable for machine)')
-@click.argument('config_path', metavar='CONFIG', type=click.Path(exists=True))
-@click.argument('files', nargs=-1, type=click.Path(exists=True))
+@click.option(
+    "-j",
+    "--jobs",
+    default=None,
+    type=int,
+    help="parallel threads to use (default: suitable for machine)",
+)
+@click.argument("config_path", metavar="CONFIG", type=click.Path(exists=True))
+@click.argument("files", nargs=-1, type=click.Path(exists=True))
 def brench(config_path, files, jobs):
-    """Run a batch of benchmarks and emit a CSV of results.
-    """
+    """Run a batch of benchmarks and emit a CSV of results."""
     with open(config_path) as f:
         config = tomlkit.loads(f.read())
 
     # Use configured file list, if none is specified via the CLI.
-    if not files and 'benchmarks' in config:
-        files = glob.glob(config['benchmarks'], recursive=True)
+    if not files and "benchmarks" in config:
+        files = glob.glob(config["benchmarks"], recursive=True)
 
-    timeout = config.get('timeout', 5)
+    timeout = config.get("timeout", 5)
 
     with futures.ThreadPoolExecutor(max_workers=jobs) as pool:
         # Submit jobs.
         futs = {}
         for fn in files:
-            for name, run in config['runs'].items():
-                futs[(fn, name)] = pool.submit(run_bench, run['pipeline'], fn,
-                                               timeout)
+            for name, run in config["runs"].items():
+                futs[(fn, name)] = pool.submit(run_bench, run["pipeline"], fn, timeout)
 
         # Collect results and print CSV.
         writer = csv.writer(sys.stdout)
-        writer.writerow(['benchmark', 'run', 'result'])
+        writer.writerow(["benchmark", "run", "result"])
         for fn in files:
             first_out = None
-            for name in config['runs']:
+            for name in config["runs"]:
                 try:
                     stdout, stderr = futs[(fn, name)].result()
                 except subprocess.TimeoutExpired:
-                    stdout, stderr = '', ''
-                    status = 'timeout'
+                    stdout, stderr = "", ""
+                    status = "timeout"
                 else:
                     status = None
 
@@ -112,21 +110,23 @@ def brench(config_path, files, jobs):
                 if first_out is None:
                     first_out = stdout
                 elif stdout != first_out and not status:
-                    status = 'incorrect'
+                    status = "incorrect"
 
                 # Extract the figure of merit.
-                result = get_result([stdout, stderr], config['extract'])
+                result = get_result([stdout, stderr], config["extract"])
                 if not result and not status:
-                    status = 'missing'
+                    status = "missing"
 
                 # Report the result.
                 bench, _ = os.path.splitext(os.path.basename(fn))
-                writer.writerow([
-                    bench,
-                    name,
-                    status if status else result,
-                ])
+                writer.writerow(
+                    [
+                        bench,
+                        name,
+                        status if status else result,
+                    ]
+                )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     brench()
