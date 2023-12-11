b62791667fea0211bf3ccbde840d0fcf06d97be4
Omkar Bhalerao <obhal6543@gmail.com>
2023-12-11 11:12:14 -0500

    add sharedenv formatter

--- src/shared_env.rs
+++ src/shared_env.rs
@@ -1,17 +1,38 @@
 use num_traits::{One, Zero};
 use std::cmp::Ordering;
 use std::ops::Rem;
-
+use std::fmt;
 use num_integer::Integer;
 
 use bitvec::prelude::*;
 use num_bigint::{BigInt, BigUint};
 use std::iter::once;
+use std::mem::size_of;
 
 #[derive(Debug)]
 pub struct SharedEnvironment {
   shared_bits: BitVec<usize, Lsb0>, // RI: integers are little-endian
   offsets: Vec<usize>,              // offsets[i] = start of node i
+}
+
+impl fmt::Display for SharedEnvironment {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+
+    write!(f, "\nEnvironment:\n");
+
+    for i in 0..self.offsets.len()-1 {
+      if self.offsets[i] == self.offsets[i+1] {
+        writeln!(f, "{} : _", i);
+      } else {
+        if self.offsets[i+1] - self.offsets[i] > size_of::<usize>()*8 {
+          writeln!(f, "{} : too large to display", i);
+        }
+        writeln!(f, "{} : {}", i, SharedEnvironment::slice_to_usize(&self.shared_bits[self.offsets[i]..self.offsets[i+1]]));
+      }
+    }
+
+    Ok(())
+  }
 }
 
 impl SharedEnvironment {
